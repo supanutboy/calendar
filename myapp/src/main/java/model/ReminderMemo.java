386be29982e93486332ac9940caf746cccfffb0f
supanutboy <supanut.thi@ku.th>
2017-10-19 20:14:00 +0700

    5810451101

---
+++ myapp/src/main/java/model/ReminderMemo.java
@@ -0,0 +1,46 @@
+package model;
+
+public class ReminderMemo {
+	private  String subject;
+	private  String info;
+	private  String day;
+	private  String month;
+	private  String year;
+	public ReminderMemo(String subject,String info,String day,String month,String year) {
+		this.subject=subject;
+		this.day=day;
+		this.month=month;
+		this.year=year;
+		this.info=info;
+	}
+	public  String getSubject() {
+		return subject;
+	}
+	void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public  String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public  String getDay() {
+		return day;
+	}
+	void setDay(String day) {
+		this.day = day;
+	}
+	public  String getMonth() {
+		return month;
+	}
+	void setMonth(String month) {
+		this.month = month;
+	}
+	public  String getYear() {
+		return year;
+	}
+	void setYear(String year) {
+		this.year = year;
+	}
+}
